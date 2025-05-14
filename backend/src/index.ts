1480a198c9d144e8fec5e18671d227a3df320f09
digveshparab <digveshparab123@gmail.com>
2025-05-14 22:14:41 +0530

    backend intrgation changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import bookingRouter from './routers/booking';
 const app = express();
+import cors from 'cors'
 
+
+cors
 app.use(express.json())
+app.use(cors())
 app.use('/book',bookingRouter);
 
 app.listen(5000)
